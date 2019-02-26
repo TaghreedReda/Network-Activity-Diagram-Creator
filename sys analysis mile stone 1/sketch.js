39bad8400b4e7a0a236c1685ed5ed140cd5559a5
TaghreedReda <taghreedzredaz@gmail.com>
2019-02-26 17:48:15 +0200

    initial design

--- sys analysis mile stone 1/sketch.js
+++ sys analysis mile stone 1/sketch.js
@@ -1,23 +1,36 @@
-var backk, x=-400,y=400;
 
-function preload(){
 
- backk=loadAnimation("1.png","2.png");
-}
+var nameP , durationV ,nameV,durationP,appendicesV,appendicesP;
 
-function setup() {
+function setup() 
+{
   createCanvas(800,800);
- 
+  //1-name
+  nameP=createElement('h3', 'Activity Name : ');
+  nameP.position(10,45);
+  nameV=createInput();
+  nameV.position(188,65);
+
+  //2-duration
+  durationP=createElement('h3', 'Activity Duration : ');
+  durationP.position(10,85);
+  durationV=createInput();
+  durationV.position(188,105);
+
+  //3-appendices
+  appendicesP=createElement('h3', 'Activity Appendices : ');
+  appendicesP.position(10,125);
+  appendicesV=createInput();
+  appendicesV.position(188,145);
+
+
 }
 
-function draw() {
- background(0);
 
-  if(x<700){
-    animation(backk,x,y);
-     x+=3;
-  }
-  
- 
+
+
+function draw()
+{
+ background(255); 
 
 }
